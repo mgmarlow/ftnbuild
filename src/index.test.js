aab47e41be782329a367abc4bf165bc8da241d8b
Graham Marlow <mgmarlow@hey.com>
2021-10-23 12:26:32 -0700

    Test against bigfish as well

--- src/index.test.js
+++ src/index.test.js
@@ -3,8 +3,15 @@ import fountain from './index'
 
 describe('fountain', () => {
   describe('#parse', () => {
-    it('should run against fixture', () => {
+    it('should match small fixture', () => {
       const sample = fs.readFileSync('./samples/brick&steel.fountain', {
+        encoding: 'utf-8',
+      })
+      expect(fountain.parse(sample, true)).toMatchSnapshot()
+    })
+
+    it('should match large fixture', () => {
+      const sample = fs.readFileSync('./samples/bigfish.fountain', {
         encoding: 'utf-8',
       })
       expect(fountain.parse(sample, true)).toMatchSnapshot()
@@ -12,8 +19,15 @@ describe('fountain', () => {
   })
 
   describe('#tokenize', () => {
-    it('should run against fixture', () => {
+    it('should match small fixture', () => {
       const sample = fs.readFileSync('./samples/brick&steel.fountain', {
+        encoding: 'utf-8',
+      })
+      expect(fountain.tokenize(sample)).toMatchSnapshot()
+    })
+
+    it('should match large fixture', () => {
+      const sample = fs.readFileSync('./samples/bigfish.fountain', {
         encoding: 'utf-8',
       })
       expect(fountain.tokenize(sample)).toMatchSnapshot()
